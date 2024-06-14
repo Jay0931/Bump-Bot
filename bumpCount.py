f3afdc2cf8a0595edec4285cfb1d7500d8be1188
Jay0931 <gujustin27@gmail.com>
2024-06-13 17:09:25 -0700

    Create bumpCount.py

---
+++ bumpCount.py
@@ -0,0 +1,69 @@
+import discord
+from discord.ext import tasks, commands
+from dotenv import load_dotenv
+import os
+
+# Load the environment variables from the .env file
+load_dotenv()
+TOKEN = os.getenv('DISCORD_BOT_TOKEN')
+LEADERBOARD_CHANNEL_ID = int(os.getenv('LEADERBOARD_CHANNEL_ID'))
+
+# Create an instance of a bot
+intents = discord.Intents.default()
+intents.messages = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+# Dictionary to keep track of command usage
+command_usage = {}
+
+# Message ID of the leaderboard message (will be updated once the message is sent)
+leaderboard_message_id = None
+
+# Format the leaderboard message
+def format_leaderboard():
+    sorted_users = sorted(command_usage.items(), key=lambda x: x[1], reverse=True)
+    leaderboard_message = "**Bump Leaderboard**\n\n"
+    for user_id, count in sorted_users:
+        leaderboard_message += f"<@{user_id}> - {count}\n"
+    return leaderboard_message
+
+# Event that triggers when the bot is ready
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+    await setup_leaderboard()
+
+# Function to setup the leaderboard message
+async def setup_leaderboard():
+    global leaderboard_message_id
+    channel = bot.get_channel(LEADERBOARD_CHANNEL_ID)
+    if channel is not None:
+        leaderboard_message = format_leaderboard()
+        message = await channel.send(leaderboard_message)
+        leaderboard_message_id = message.id
+
+# Event that triggers on every new message
+@bot.event
+async def on_message(message):
+    if message.content == "/bump":
+        user_id = message.author.id
+        if user_id not in command_usage:
+            command_usage[user_id] = 0
+        command_usage[user_id] += 1
+
+        await update_leaderboard()
+    
+    await bot.process_commands(message)
+
+# Function to update the leaderboard message
+async def update_leaderboard():
+    global leaderboard_message_id
+    channel = bot.get_channel(LEADERBOARD_CHANNEL_ID)
+    if channel is not None:
+        leaderboard_message = format_leaderboard()
+        if leaderboard_message_id is not None:
+            message = await channel.fetch_message(leaderboard_message_id)
+            await message.edit(content=leaderboard_message)
+
+# Run the bot with the token
+bot.run(TOKEN)
